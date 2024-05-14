93088ab278a67dbffd50a7e9aded931a1bc3a064
gogin-png <den.sukhanov.99@mail.ru>
2024-05-14 15:38:54 +0300

    help

---
+++ YOLO/Chapter 1/YOLO.py
@@ -0,0 +1,12 @@
+import cv2
+from ultralytics import YOLO
+
+
+# Создается экземпляр модели Chapter 1, загружая предобученную модель из файла 'yolov8n.pt' (можно указать путь для скачки).
+model = YOLO('/YOLO/YoloModels/yolov8n.pt')
+
+# Модель используется для обнаружения объектов на изображении, указанном в пути ' Результаты обнаружения сохраняются в переменной results.
+results = model("/home/denis/PycharmProjects/ObjectDetection/images/1.jpg",show=True)
+
+# Ожидается нажатие клавиши (любой) для продолжения выполнения программы после отображения результатов обнаружения на изображении.
+cv2.waitKey(0)
---
+++ YOLO/main.py
@@ -0,0 +1,1 @@
+for ultralytics import YOLO
